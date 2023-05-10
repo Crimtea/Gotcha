2d996411b9a2480defa2e1ba253fadab250d079f
Crimtea <Crimtea@outlook.com>
2023-05-10 16:20:57 +0800

    update

--- js/index.js
+++ js/index.js
@@ -22,7 +22,7 @@ lightRecharge.addEventListener("click", function() {
 airRecharge.addEventListener("click", function() {
 	if (var_air.value == ''){
 		alert("请输入金额");
-	}else if (var_light.value < 6){
+	}else if (var_air.value < 6){
 		alert("充值金额不可低于6元")
 	}else {
 		window.open('http://dk.suet.edu.cn/dk_pay/wx_pay.php?data={%22xiaoqu%22:%22ba84c2fb-5bf7-459e-976d-bdee08ec036f%22,%22louzuo%22:%222aac1ebb-4d96-4878-8fc5-35b5af774e61%22,%22louceng%22:%2212%22,%22sushe%22:%223983f93d-6002-4045-a55d-1da2efdc2794%22,%22price%22:%22'+(var_air.value - var_air.value%6)+'%22}','_self');
