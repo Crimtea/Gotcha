3efa70bf14e1688d0a591f3cd625cfa3ba823172
Crimtea <Crimtea@outlook.com>
2023-06-14 16:39:57 +0800

    update

--- js/genshin.js
+++ js/genshin.js
@@ -34,11 +34,10 @@ function prob_5(amass_5_int) {
 }
 
 // main
-function main(own, stone, amass_4, amass_5,
+function main(own, amass_4, amass_5,
     nextTrue_4, nextTrue_5,
     code = 0, codeTrue,
     p1, p2, p3) {
-    own = own + parseInt(stone / 160);
     var timer_3 = 0;
     var list_5 = [];
     var up_5 = 0;
@@ -47,13 +46,13 @@ function main(own, stone, amass_4, amass_5,
     var all_num = own;
 
     if (own <= 0) {
-        return 0;
+        return "不足1抽";
     }
 
     while (own > 0) {
         own = own - 1;
         if (prob_5(amass_5 + 1)) {
-            list_5.push(amass_5);
+            list_5.push(Number(amass_5));
             amass_4 += 1;
             amass_5 = 0;
             code += 10;
@@ -64,48 +63,48 @@ function main(own, stone, amass_4, amass_5,
                 not_5 += 1;
                 nextTrue_5 = true;
             }
-        } else if (prob_4(amass_4 + 1)) {
-            code += 2;
-            amass_4 = 0;
-            amass_5 += 1;
-            if (odds(0.5) || nextTrue_4) {
+        } else if (prob_4(amass_4 + 1)) {  // 抽到四星
+            code += 2;  // 星辉+2
+            amass_4 = 0;  // 四星垫卡归零
+            amass_5 += 1;  // 五星垫卡+1
+            if (odds(0.5) || nextTrue_4) {  // 50%UP或使用大保底获得四星
                 var p_num = Math.floor(Math.random() * 3) + 1;
                 switch (p_num) {
-                    case 1:
-                        if (p1 < 7) {
-                            p1 += 1;
-                        } else {
-                            code += 3;
+                    case 1:  // 抽到四星角色一
+                        if (p1 < 6) {  //命座<6时
+                            p1 += 1;  // 命座+1
+                        } else {  // 命座>=6时
+                            code += 3;  // 星辉额外+3，总计+5
                         }
                         break;
                     case 2:
-                        if (p2 < 7) {
+                        if (p2 < 6) {
                             p2 += 1;
                         } else {
                             code += 3;
                         }
                         break;
                     case 3:
-                        if (p3 < 7) {
+                        if (p3 < 6) {
                             p3 += 1;
                         } else {
                             code += 3;
                         }
                         break;
                 }
-                nextTrue_4 = false;
-            } else {
-                not_4 += 1;
-                nextTrue_4 = true;
+                nextTrue_4 = false;  // 获得四星UP后进入四星小保底
+            } else {  // 抽到四星非UP
+                not_4 += 1;  // 统计四星非UP数量
+                nextTrue_4 = true;  // 进入四星大保底
             }
-        } else {
-            amass_4 += 1;
-            amass_5 += 1;
-            timer_3 += 1;
+        } else {  // 抽到三星
+            amass_4 += 1;  // 四星垫卡+1
+            amass_5 += 1;  // 五星垫卡+1
+            timer_3 += 1;  // 统计三星卡数量
         }
         if (codeTrue && code >= 5) {
-            own += parseInt(stone / 160);
-            all_num += parseInt(stone / 160);
+            own += parseInt(code / 5);
+            all_num += parseInt(code / 5);
             code = code % 5;
         }
     }
@@ -115,12 +114,16 @@ function main(own, stone, amass_4, amass_5,
     var up_4 = p1 + p2 + p3;
     var timer_4 = up_4 + not_4;
     var timer_5 = up_5 + not_5;
-    var list_amass = [timer_3, timer_4+3, timer_5, up_4+3, up_5,
-        (((timer_5 / all_num) * 100).toFixed(2) + "%"),
-        ((list_5.reduce((total, currentValue) =>
-            total + currentValue, 0) / list_5.length))
-    ];
-    return list_amass;
+    var text_amass =`
+        三星卡数量：${timer_3}\n
+        四星卡数量：${timer_4+3}\n
+        五星卡数量：${timer_5}\n
+        四星up卡数量：${up_4+3}\n
+        五星up卡数量：${up_5}\n
+        五星占比为：${(((timer_5 / all_num) * 100).toFixed(2) + "%")}\n
+        五星平均次数：${(list_5.reduce((total, current) => total + current, 0)/list_5.length).toFixed(2)}\n`;
+//    alert(list_5);
+    return text_amass;
 
 }
 
@@ -138,11 +141,10 @@ run.onclick = function () {
     var p3 = document.getElementById("text_p3") || -1;
 
 
-
     own = own + parseInt(stone / 160); // 将原石换成纠缠之缘
-    var list_end = main(own, stone, amass_4, amass_5, nextTrue_4.value, nextTrue_5.value, code, codeTrue.value, p1, p2, p3);
-    alert(list_end);
-//    var list_test = [own, stone, amass_4, amass_5, nextTrue_4, nextTrue_5, code, codeTrue, p1, p2, p3];
-//    alert(list_test);
+    var dict_end = main(own, amass_4, amass_5, nextTrue_4.value, nextTrue_5.value, code, codeTrue.value, p1, p2, p3);
+    alert(dict_end);
+    //    var list_test = [own, stone, amass_4, amass_5, nextTrue_4, nextTrue_5, code, codeTrue, p1, p2, p3];
+    //    alert(list_test);
 
 };
