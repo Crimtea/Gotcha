88af1dc06242a0df3c5688f0a6fd8e437302687e
Crimtea <Crimtea@outlook.com>
2023-08-15 21:35:23 +0800

    update

--- js/index.js
+++ js/index.js
@@ -11,21 +11,21 @@ var airRecharge = document.getElementById("airRecharge");
 
 lightRecharge.addEventListener("click", function () {
     if (var_light.value == '') {
-        alert("请输入金额");
-    } else if (var_light.value < 6) {
-        alert("充值金额不可低于6元")
+        alert("请输入充值度数");
+    } else if (var_light.value < 1) {
+        alert("不可低于1度")
     } else {
-        window.open('http://dk.suet.edu.cn/dk_pay/wx_pay.php?data={%22xiaoqu%22:%224865894c-9415-4035-a5be-c8ceebd2702a%22,%22louzuo%22:%222b27c349-9851-45bb-b15f-09637c413dfd%22,%22louceng%22:%2212%22,%22sushe%22:%226e809e33-3b1a-4903-baa3-f2e43898c127%22,%22price%22:%22' + (var_light.value - var_light.value % 6) + '%22}', '_self');
+        window.open('http://dk.suet.edu.cn/dk_pay/wx_pay.php?data={%22xiaoqu%22:%224865894c-9415-4035-a5be-c8ceebd2702a%22,%22louzuo%22:%222b27c349-9851-45bb-b15f-09637c413dfd%22,%22louceng%22:%2212%22,%22sushe%22:%226e809e33-3b1a-4903-baa3-f2e43898c127%22,%22price%22:%22' + (var_light.value * 0.6) + '%22}', '_self');
     }
 });
 
 airRecharge.addEventListener("click", function () {
     if (var_air.value == '') {
-        alert("请输入金额");
-    } else if (var_air.value < 6) {
-        alert("充值金额不可低于6元")
+        alert("请输入充值度数");
+    } else if (var_air.value < 1) {
+        alert("不可低于1度")
     } else {
-        window.open('http://dk.suet.edu.cn/dk_pay/wx_pay.php?data={%22xiaoqu%22:%22ba84c2fb-5bf7-459e-976d-bdee08ec036f%22,%22louzuo%22:%222aac1ebb-4d96-4878-8fc5-35b5af774e61%22,%22louceng%22:%2212%22,%22sushe%22:%223983f93d-6002-4045-a55d-1da2efdc2794%22,%22price%22:%22' + (var_air.value - var_air.value % 6) + '%22}', '_self');
+        window.open('http://dk.suet.edu.cn/dk_pay/wx_pay.php?data={%22xiaoqu%22:%22ba84c2fb-5bf7-459e-976d-bdee08ec036f%22,%22louzuo%22:%222aac1ebb-4d96-4878-8fc5-35b5af774e61%22,%22louceng%22:%2212%22,%22sushe%22:%223983f93d-6002-4045-a55d-1da2efdc2794%22,%22price%22:%22' + (var_air.value * 0.6) + '%22}', '_self');
     }
 });
 
